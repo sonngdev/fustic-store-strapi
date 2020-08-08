09b4df30767f9e9b7023400251bb2b7c1d5c3b85
Nguyễn Thanh Sơn <son@gotitapp.co>
2020-08-08 14:35:20 +0700

    feat: change general config response

--- api/general-config/controllers/general-config.js
+++ api/general-config/controllers/general-config.js
@@ -4,5 +4,22 @@
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
+const { sanitizeEntity } = require('strapi-utils');
 
-module.exports = {};
+function sanitizeGeneralConfig(config) {
+  let sanitized;
+
+  sanitized = sanitizeEntity(config, { model: strapi.models['general-config'] });
+
+  delete sanitized.created_by;
+  delete sanitized.updated_by;
+
+  return sanitized;
+}
+
+module.exports = {
+  async find() {
+    const entity = await strapi.services['general-config'].find();
+    return sanitizeGeneralConfig(entity);
+  },
+};
