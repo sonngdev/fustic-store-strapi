4ff6f5d1341636f9cf4c30ceca6bf27a7c3484bc
Nguyễn Thanh Sơn <son@gotitapp.co>
2020-08-08 12:35:38 +0700

    Init commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c387269065cbab443e11e7fa5b29b100'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9c491e6f-0cac-48a2-b3fa-579f100a12ef'
+};
