49e8b91b48d1b22f81abbe4568928228cda85835
Nguyễn Thanh Sơn <son@gotitapp.co>
2020-08-08 14:47:44 +0700

    feat: change categories response

--- api/category/controllers/category.js
+++ api/category/controllers/category.js
@@ -4,5 +4,27 @@
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
+const { sanitizeEntity } = require('strapi-utils');
+const { removeUserInfo } = require('../../../utils/response');
 
-module.exports = {};
+function sanitizeCategory(category) {
+  let sanitized = category;
+
+  sanitized = sanitizeEntity(sanitized, { model: strapi.models.category });
+  sanitized = removeUserInfo(sanitized);
+
+  return sanitized;
+}
+
+module.exports = {
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.category.search(ctx.query);
+    } else {
+      entities = await strapi.services.category.find(ctx.query);
+    }
+
+    return entities.map(sanitizeCategory);
+  },
+};
