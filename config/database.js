ae6ebfff903845a9683e3c37c2cc3efee5d3176c
Nguyễn Thanh Sơn <son@gotitapp.co>
2021-03-18 10:22:48 +0700

    feat: change ssl config for database

--- config/database.js
+++ config/database.js
@@ -10,9 +10,13 @@ module.exports = ({ env }) => ({
         database: env('DATABASE_NAME', 'fustic_store_cms'),
         username: env('DATABASE_USERNAME', ''),
         password: env('DATABASE_PASSWORD', ''),
-        ssl: env.bool('DATABASE_SSL', false),
+        ssl: {
+          rejectUnauthorized: false,
+        }
       },
-      options: {}
+      options: {
+        ssl: env.bool('DATABASE_SSL', true),
+      },
     },
   },
 });
